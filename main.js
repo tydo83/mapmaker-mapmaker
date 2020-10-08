06c720c1e1d24b64f116ce21580c962bbbe39e5c
Timothy Do <tydo83@gmail.com>
2020-10-08 15:58:49 -0400

    left changeToInitials

--- main.js
+++ main.js
@@ -1,11 +1,79 @@
 /******************
  * YOUR CODE HERE *
 ******************/
+function doubleAll(array) {
+  let doubleArray = [];
+  for(let i = 0; i < array.length; i++) {
+    doubleArray.push(array[i] * 2);
+  }
+  return doubleArray;
+} 
+
+function yelledGreetings(array) {
+  let yelledArray = [];
+  for(let i = 0; i < array.length; i++) {
+    yelledArray.push(array[i] + '!');
+  }
+  return yelledArray;
+} 
+
+function absoluteValues(array) {
+  let absoluteValues = [];
+  for(let i = 0; i < array.length; i++) {
+    absoluteValues.push(Math.abs(array[i]));
+  }
+  return absoluteValues;
+} 
+
+function upperCaseFirstLetters(array) {
+  let upperCaseFirstLetters = [];
+  for(let i = 0; i < array.length; i++) {
+    upperCaseFirstLetters.push(array[i][0].toUpperCase() + array[i].slice(1).toLowerCase());
+  }
+  return upperCaseFirstLetters;
+}
+
+function changeToInitials(array) {
+  initials = [];
+  for(i = 0; i < array.length; i++) {
+    initials.push(array[i][0].toUpperCase() + array[i].indexOf(' ').toUpperCase);
+  }
+  return initials;
+}
+
+a = changeToInitials(['Colin Jaffe', 'Mesuara Kaleziq'])
+b = changeToInitials(['Larry Bird', 'Robert Parrish']) 
+a;
+b;
 
 
 
 
+function doubleOdd(array) {
+  doubleOdd = [];
+  for(i = 0; i < array.length; i++) {
+    if(Math.abs(array[i]) % 2 === 1) {
+      doubleOdd.push(array[i] * 2);
+    }
+    else {
+      doubleOdd.push(array[i]);
+    }
+  } 
+  return doubleOdd;
+}
 
+function add1ToLeft(array) {
+  addedOne = [];
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] >= 0) { 
+      addedOne.push(Number('1' + array[i]))
+    }
+    if (array[i] < 0) {
+      addedOne.push(-1 * Number('1' + Math.abs(array[i])))
+    }
+  } 
+  return addedOne;
+}
 
 /*******************************************
 * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
